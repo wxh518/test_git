c01bc721d6d2fe5db0b3aaaac812e3f05c0d2ab3
wxh518 <760769698@qq.com>
2023-06-07 22:41:15 +0800

    main commit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
 
-int Sub(int x int y)
-{
-	// main
-	// wxh  
-	return x - y;
-}
-
 int main()
 {
-	// main
 	std::cout << "learn git!" << std::endl;
 	std::cout << "main dev branch learn git!" << std::endl;
 	std::cin.get();
